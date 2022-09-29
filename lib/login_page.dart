8741d7890815566276d6a4d73d3b9449d9c4666f
SRS <srsms43@gmail.com>
2022-09-29 14:02:21 +0530

    added button animation

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,15 +1,21 @@
 import 'package:dummy/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatefulWidget{
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name='';
+  bool changeButton=false;
   @override
 Widget build(BuildContext context){
   return Material(
     color:Colors.white,
- 
-   
     child: SingleChildScrollView(
       child: Column(
+        
         children: [
           SizedBox(
             width: 100,
@@ -26,45 +32,82 @@ Widget build(BuildContext context){
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                Text("Welcome",
-                style: TextStyle(
+                Text("Welcome $name",
+                style: const TextStyle(
                   color: Colors.deepPurple,
                   fontSize: 30,
                   fontWeight: FontWeight.bold
                 ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 25,
                 ),
         TextFormField(
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: "Enter You User Name",
-                  labelText: "Username"
+                  labelText: "Username",
                 ),
+                onChanged: (value) {
+                  name=value;
+                  setState(() {
+                });
+                },
         ),
         TextFormField(
                 obscureText: true,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: "Enter You Password",
                   labelText: "Password",
                   
                 ),
         ),
-        SizedBox(
+        const SizedBox(
                 height:20
         ),
-        ElevatedButton(onPressed: (){
-                Navigator.pushNamed(context, MyRoutes.homeRoute);
-        },
-         child: Text('Login'),
-         style: TextButton.styleFrom(
-          minimumSize: Size(150, 40)
-         ),
-         ),
+        // ElevatedButton(onPressed: (){
+        //         Navigator.pushNamed(context, MyRoutes.homeRoute);
+        // },
+        //  child: Text('Login'),
+        //  style: TextButton.styleFrom(
+        //   minimumSize: Size(150, 40)
+        //  ),
+        //  ),
+        InkWell(
+
+          onTap:  () async{
+            setState(() {
+               changeButton=true;
+            });
+
+            await Future.delayed(Duration(milliseconds: 800));
+             Navigator.pushNamed(context, MyRoutes.homeRoute);
+          },
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 800),
+            width:changeButton?50:150,
+            height:50,
+            alignment: Alignment.center,
+            child: changeButton
+            ?const Icon(Icons.done,
+            color:Colors.white)
+            :const Text("Login",
+            style: TextStyle(
+              color:Colors.white,
+              fontSize: 16
+            ),
+            ),
+            decoration: BoxDecoration(
+              color:Colors.deepPurple,
+            //   shape: changeButton
+            //   ? BoxShape.circle
+            //   :BoxShape.rectangle,
+             borderRadius: BorderRadius.circular(changeButton?50:10)
+            ) ,
+          ),
+        )
               ],
             ),
           )
-    
         ],
     
       ),
@@ -72,5 +115,4 @@ Widget build(BuildContext context){
   
   );
 }
-
 }
